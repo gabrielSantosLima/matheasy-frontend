85edc37fb98862e27d6402a9b8a595e0657e70b1
Gabriel Lima <bielsantoslima10@gmail.com>
2020-08-23 15:01:46 -0400

    Chat finalizado

--- Telas/Chat/script.js
+++ Telas/Chat/script.js
@@ -1,26 +1,15 @@
 const userId = $(document.body).data('id') 
+let contatoId;
 const from = $(document.body).data('name') 
 const sock = new SockJS('http://localhost:8080/chat')
 const stompClient = Stomp.over(sock);
+
+console.log(sock)
+
 const Chat = {
-  
-  //conecta ao chat 
+  //Conecta ao chat 
   connect: () =>{
-    
-    stompClient.connect({}, frame => {
-      console.log(sock)
-      
-      stompClient.subscribe('/queue/public', resp => {
-        const message = JSON.parse(resp.body)
-
-        if(message.userId === userId){
-          return;
-        }
-
-        receiveMessage(message);
-
-      });
-    
+    stompClient.connect({}, frame => {  
     }, error => console.log(error))
     
     console.log(`[chat] Usuário ${userId} Conectou!`);
@@ -37,46 +26,119 @@ const Chat = {
     event.preventDefault();
     const content = $('#input').val();
 
+    if(!content){
+      return;
+    }
+
     const message = {
       from,
       content,
       userId
     }
 
-    const resp = stompClient.send('/app/chat',{}, JSON.stringify(message));
+    stompClient.send(
+      `/app/chat/${contatoId}`,
+      {}, 
+      JSON.stringify(message)
+    );
 
     $('#input').val('')
     addMessage(message)
-  } 
+  },
+
+  //Reseta mensagens do chat
+  reset: () =>{
+    $('.chat').html('')
+  }
 }
 // Message -> from , time, content, to
 
 function addMessage(message){
   const messageDOM = `
   <div class="my-message">
-  <img src="./../../assets/imagem-teste.jpg" alt="User" class="logo">
+  <img src="http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Free-Download.png" alt="User" class="logo">
   <strong>${message.from}</strong>
   <p>${message.content}</p>
   </div>
   `
   $('.chat').append(messageDOM);
+  endScrollChat();
 }
 
 function receiveMessage(message){
   const messageDOM = `
   <div class="other-message">
-  <img src="./../../assets/imagem-teste.jpg" alt="User" class="logo">
+  <img src="http://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Free-Download.png" alt="User" class="logo">
   <strong>${message.from}</strong>
   <p>${message.content}</p>
   </div>
   `
   $('.chat').append(messageDOM);
+  endScrollChat();
+}
+
+async function loadMessages(){
+  const resp = await fetch(`http://localhost:8080/chat/${userId}/${contatoId}`)
+  const messages = await resp.json();
+
+  messages.map(message => {
+    if(message.userId === userId){
+      receiveMessage(message)
+    }else{
+      addMessage(message)
+    }
+  })
+}
+
+function userSelect(event){
+  const contato = $(this) 
+  
+  //Altera cor de contato
+  $('.contato').removeClass('select')
+  contato.addClass('select')
+
+  contatoId = contato.data('contatoId');
+
+  showContato();
+}
+
+function showContato(){
+  //Quando clicado, faz aparecer o chat com as mensagens
+  $('#form').removeClass('hide')
+  $('.chat').removeClass('hide')
+  
+  //Carrega as mensagens
+  Chat.reset(); //Por enquanto enquanto n carrega mensagens do banco
+  
+  subscribe(`/queue/public/${userId}/${contatoId}`);
+  loadMessages();
+}
+
+function subscribe(channel){
+  stompClient.subscribe(channel, resp => {
+    const message = JSON.parse(resp.body)
+  
+    if(message.userId === userId){
+      return;
+    }
+  
+    receiveMessage(message);
+  
+  });
 }
 
 function addEvents(){
   $('#form').submit(Chat.sendMessage)
+  $('.contato').on('click', userSelect)
   $(document).ready(Chat.connect)
-  $(document).ready(Chat.loadMessages)
+}
+
+function endScrollChat(){
+  const totalHeight = $('.chat')[0].scrollHeight
+
+  console.log(totalHeight)
+
+  $('.chat').scrollTop(totalHeight)
 }
 
 function init(){
